e5529e63e0e8d79b81d90b8571986199344cf23c
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-06-06 14:07:34 -0700

    headers

--- netlify/functions/pixiFetch.js
+++ netlify/functions/pixiFetch.js
@@ -4,7 +4,7 @@ exports.handler = async function (event, context) {
    console.log(event.queryStringParameters);
 
    try {
-   const { term } = event.queryStringParameters;
+    const { term } = event.queryStringParameters;
       console.log(term);
       const response = await fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`);
       const data = await response.json();
@@ -13,11 +13,17 @@ exports.handler = async function (event, context) {
       console.log(images);
       return {
         statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
         body: JSON.stringify({ images }),
       };
    } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify({ error })
     }
    }
